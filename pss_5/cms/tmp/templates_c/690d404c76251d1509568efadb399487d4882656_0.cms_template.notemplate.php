65cb92f2f5c7d9ff1c4273b98f60a4b35dbfe0b4
Julia Ksi <101068886+juliaksi@users.noreply.github.com>
2023-05-07 23:18:54 +0200

    Add files via upload

---
+++ pss_5/cms/tmp/templates_c/690d404c76251d1509568efadb399487d4882656_0.cms_template.notemplate.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.31, created on 2023-05-07 23:10:38
+  from "cms_template:notemplate" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.31',
+  'unifunc' => 'content_645813cef2b410_61266215',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '690d404c76251d1509568efadb399487d4882656' => 
+    array (
+      0 => 'cms_template:notemplate',
+      1 => 1683493838,
+      2 => 'cms_template',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_645813cef2b410_61266215 (Smarty_Internal_Template $_smarty_tpl) {
+CMS_Content_Block::smarty_internal_fetch_contentblock(array(),$_smarty_tpl);
+}
+}
